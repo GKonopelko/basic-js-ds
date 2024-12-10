f340d8caf9da46ba9c8bc90850748480717bd94d
Grigori Konopelko <grigori.konopelko@gmail.com>
2024-12-10 14:46:40 +0300

    feat: add remove and stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -23,12 +23,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if(l.value === k) {
+    l = l.next;
+  }
+  let previousNode;
+  let currentNode = l;
+
+  while (currentNode) {
+    if (currentNode.value === k) {
+      previousNode.next = currentNode.next;
+    } else {
+      previousNode = currentNode;
+    }
+    currentNode = currentNode.next;
+  }
+  return l;
 }
-
-
 
 
 
